6ed8f74fc51aa73317302d15933cdf248eec9ade
y.takaki <46508995+takaki-n-create@users.noreply.github.com>
2024-11-27 11:47:54 +0900

    Update ZipEntry.Extract.cs
    DotNetZip.Semverd project の DotNetZip.Semverd におけるパストラバーサルの脆弱性
    https://gist.github.com/thomas-chauchefoin-bentley-systems/855218959116f870f08857cce2aec731

--- src/Zip.Shared/ZipEntry.Extract.cs
+++ src/Zip.Shared/ZipEntry.Extract.cs
@@ -1390,6 +1390,15 @@ namespace Ionic.Zip
             // workitem 10639
             outFileName = outFileName.Replace('/', Path.DirectorySeparatorChar);
 
+            // Resolve any directory traversal sequence and compare the result with the intended base directory
+            // where the file or folder will be created.
+            var canonicalOutPath = Path.GetFullPath(outFileName);
+            var canonicalBaseDir = Path.GetFullPath(baseDir);
+            if (!canonicalOutPath.StartsWith(canonicalBaseDir, StringComparison.OrdinalIgnoreCase))
+            {
+                throw new IOException(string.Format("Extracting {0} would write to {1}, outside of {2}; rejecting.", outFileName, canonicalOutPath, canonicalBaseDir));
+            }
+            
             // check if it is a directory
             if (IsDirectory || FileName.EndsWith("/"))
             {
